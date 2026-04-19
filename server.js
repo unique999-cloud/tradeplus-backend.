a92abb5fc655a23d90ad2ff9c85e6b11b34dc46f
unique999-cloud <orosevwotuunique@gmail.com>
2026-04-19 19:25:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,8 @@ const app = express();
 // ============================================================
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Max-Age', '86400');
-  if (req.method === 'OPTIONS') {
-    res.status(204).end();
-    return;
-  }
   next();
 });
 app.use(express.json());
